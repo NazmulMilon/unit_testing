cbea443400c27bb4ceb513e5ff2cd528cb62daeb
nazmulmilon <milonsarker73@gmail.com>
2022-09-13 12:32:48 +0600

    unittst in action

--- PyTest_fixtures.py
+++ PyTest_fixtures.py
@@ -26,7 +26,7 @@ def test_negative_case(rectangle):
     print(rectangle.width)
     rectangle.set_width(-1)
     rectangle.set_height(2)
-    assert rectangle.get_area() == -2, "incorrect negative output"
+    assert rectangle.get_area() == -1, "incorrect negative output"
 
 
 def main():
